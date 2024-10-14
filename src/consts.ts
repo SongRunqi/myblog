93e40df2ccac4f7ca7a1f463d9b20f716fab73a8
songyitian <yitiansong4@gmail.com>
2024-10-14 11:09:26 +0800

    认证follow源

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "一天的博客";
-export const SITE_DESCRIPTION = "欢迎来到我的博客";
+export const SITE_DESCRIPTION = "feedId:68487064777779200+userId:60497441312444416";
